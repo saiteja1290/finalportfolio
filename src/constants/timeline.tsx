5d031af80b0f546a653d236dcf43bdf1740370f2
saiteja.palegar@gmail.com <saiteja.palegar@gmail.com>
2024-08-30 12:06:58 +0530

    description

--- src/constants/timeline.tsx
+++ src/constants/timeline.tsx
@@ -3,7 +3,7 @@ export const timeline = [
     company: "Defence Research and Development Organisation (DRDO)",
     title: "Machine Learning Intern",
     date: "October 2023 - December 2023",
-    // description: "Building the future of e-commerce",
+    description: "GANs for Radar Signal Classification",
     responsibilities: [
       "Utilized Generative Adversarial Networks (GANs) to enhance radar signal classification, resulting in a substantial improvement in model performance by 20 percent",
       "Generated a volume of synthetic radar samples using Generative Adversarial Networks, totaling 10,000, which considerably increased the diversity and volume of the dataset."
